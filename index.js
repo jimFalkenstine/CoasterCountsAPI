d65b5ceca99f148b70b74b16da09f6690e07fced
jimFalkenstine <jim.falkenstine@gmail.com>
2022-03-14 21:55:01 -0400

    Added GET/id functionality

--- index.js
+++ index.js
@@ -21,7 +21,34 @@ router.get('/', function (req, res, next) {
   });
 });
 
-// Configure router so all routes are prefixed with /api/v1
+// Create GET/id to return a single coaster
+router.get('/:id', function (req, res, next) {
+  coasterRepo.getById(req.params.id, function (data) {
+    if (data) {
+      res.status(200).json({
+        "status": 200,
+        "statusText": "OK",
+        "message": "Single coaster retrieved.",
+        "data": data
+      });
+    }
+    else {
+      res.status(404).send({
+        "status": 404,
+        "statusText": "Not Found",
+        "message": "The coaster '" + req.params.id + "' could not be found.",
+        "error": {
+          "code": "NOT_FOUND",
+          "message": "The coaster '" + req.params.id + "' could not be found."
+        }
+      });
+    }
+  }, function(err) {
+    next(err);
+  });
+});
+
+// Configure router so all routes are prefixed with /api/
 app.use('/api/', router);
 
 // Create server to listen on port 5000
--- repos/coasterRepo.js
+++ repos/coasterRepo.js
@@ -5,14 +5,26 @@ const FILE_NAME = './assets/coasters.json';
 let coasterRepo = {
     get: function(resolve, reject) {
         fs.readFile(FILE_NAME, function (err, data) {
-            if (err) {
-                reject(err);
-            }
-            else {
-                resolve(JSON.parse(data));
-            }
+          if (err) {
+            reject(err);
+          }
+          else {
+            resolve(JSON.parse(data));
+          }
+        });
+    },
+    getById: function (id, resolve, reject) {
+        fs.readFile(FILE_NAME, function (err, data) {
+          if (err) {
+            reject(err);
+          }
+          else {
+            let coaster = JSON.parse(data).find(c => c.id == id);
+            resolve(coaster);
+          }
         });
     }
+
 };
   
   module.exports = coasterRepo;
