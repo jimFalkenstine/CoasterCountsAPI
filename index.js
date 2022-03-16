c20ddd88c391e0d019727f1ac51a8146cf328550
jimFalkenstine <jim.falkenstine@gmail.com>
2022-03-15 21:24:24 -0400

    Created POST, PUT, DELETE, & PATCH Endpoints.

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ let app = express();
 // Use the express Router object
 let router = express.Router();
 
+// Configure middleware to support JSON data parsing in request object
+app.use(express.json());
+
 
 // Create GET to return a list of all coasters
 router.get('/', function (req, res, next) {
@@ -66,6 +69,106 @@ router.get('/:id', function (req, res, next) {
     next(err);
   });
 });
+
+router.post('/', function (req, res, next) {
+  coasterRepo.insert(req.body, function(data) {
+    res.status(201).json({
+      "status": 201,
+      "statusText": "Created",
+      "message": "New Coaster Added.",
+      "data": data
+    });
+  }, function(err) {
+    next(err);
+  });
+})
+
+router.put('/:id', function (req, res, next) {
+  coasterRepo.getById(req.params.id, function (data) {
+    if (data) {
+      // Attempt to update the data
+      coasterRepo.update(req.body, req.params.id, function (data) {
+        res.status(200).json({
+          "status": 200,
+          "statusText": "OK",
+          "message": "Coaster '" + req.params.id + "' updated.",
+          "data": data
+        });
+      });
+    }
+    else {
+      res.status(404).send({
+        "status": 404,
+        "statusText": "Not Found",
+        "message": "The coaster '" + req.params.id + "' could not be found.",
+        "error": {
+          "code": "NOT_FOUND",
+          "message": "The coaster '" + req.params.id + "' could not be found."
+        }
+      });
+    }
+  }, function(err) {
+    next(err);
+  });
+})
+
+router.delete('/:id', function (req, res, next) {
+  coasterRepo.getById(req.params.id, function (data) {
+    if (data) {
+      // Attempt to delete the data
+      coasterRepo.delete(req.params.id, function (data) {
+        res.status(200).json({
+          "status": 200,
+          "statusText": "OK",
+          "message": "The coaster '" + req.params.id + "' is deleted.",
+          "data": "Coaster '" + req.params.id + "' deleted."
+        });
+      });
+    }
+    else {
+      res.status(404).send({
+        "status": 404,
+        "statusText": "Not Found",
+        "message": "The coaster '" + req.params.id + "' could not be found.",
+        "error": {
+          "code": "NOT_FOUND",
+          "message": "The coaster '" + req.params.id + "' could not be found."
+        }
+      });
+    }
+  }, function(err) {
+    next(err);
+  });
+})
+
+router.patch('/:id', function (req, res, next) {
+  coasterRepo.getById(req.params.id, function (data) {
+    if (data) {
+      // Attempt to update the data
+      coasterRepo.update(req.body, req.params.id, function (data) {
+        res.status(200).json({
+          "status": 200,
+          "statusText": "OK",
+          "message": "Coaster '" + req.params.id + "' patched.",
+          "data": data
+        });
+      });
+    }
+    else {
+      res.status(404).send({
+        "status": 404,
+        "statusText": "Not Found",
+        "message": "The coaster '" + req.params.id + "' could not be found.",
+        "error": {
+          "code": "NOT_FOUND",
+          "message": "The coaster '" + req.params.id + "' could not be found."
+        }
+      });
+    }
+  }, function (err) {
+    next(err);
+  });
+})
 
 // Configure router so all routes are prefixed with /api/
 app.use('/api/', router);
--- repos/coasterRepo.js
+++ repos/coasterRepo.js
@@ -35,13 +35,77 @@ let coasterRepo = {
         if (searchObject) {
           coasters = coasters.filter(
             c => (searchObject.id ? c.id == searchObject.id : true) &&
-              (searchObject.name ? c.name.toLowerCase().indexOf(searchObject.name) >= 0 : true));
+              (searchObject.name ? c.name.toLowerCase().indexOf(searchObject.name.toLowerCase()) >= 0 : true));
         }
 
         resolve(coasters);
       }
     });
+  },
+  insert: function (newData, resolve, reject) {
+    fs.readFile(FILE_NAME, function (err, data) {
+      if (err) {
+        reject(err);
+      }
+      else {
+        let coasters = JSON.parse(data);
+        coasters.push(newData);
+        fs.writeFile(FILE_NAME, JSON.stringify(coasters), function (err) {
+          if (err) {
+            reject(err);
+          }
+          else {
+            resolve(newData);
+          }
+        });
+      }
+    });
+  },
+  update: function (newData, id, resolve, reject) {
+    fs.readFile(FILE_NAME, function (err, data) {
+      if (err) {
+        reject(err);
+      }
+      else {
+        let coasters = JSON.parse(data);
+        let coaster = coasters.find(c => c.id == id);
+        if (coaster) {
+          Object.assign(coaster, newData);
+          fs.writeFile(FILE_NAME, JSON.stringify(coasters), function (err) {
+            if (err) {
+              reject(err);
+            }
+            else {
+              resolve(newData);
+            }
+          });
+        }
+      }
+    });
+  },
+  delete: function (id, resolve, reject) {
+    fs.readFile(FILE_NAME, function (err, data) {
+      if (err) {
+        reject(err);
+      }
+      else {
+        let coasters = JSON.parse(data);
+        let index = coasters.findIndex(c => c.id == id);
+        if (index != -1) {
+          coasters.splice(index, 1);
+          fs.writeFile(FILE_NAME, JSON.stringify(coasters), function (err) {
+            if (err) {
+              reject(err);
+            }
+            else {
+              resolve(index);
+            }
+          });
+        }
+      }
+    });
   }
+
 };
 
 module.exports = coasterRepo;
